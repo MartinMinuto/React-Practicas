cfb77e0645a6eb8819bbfb8b5816eb0310019b64
MartinMinuto <martin.minuto1@gmail.com>
2023-03-27 18:35:15 -0300

    Config 18:35

--- src/ItemList/ItemList.jsx
+++ src/ItemList/ItemList.jsx
@@ -1,6 +1,60 @@
 import { useEffect, useState } from "react";
+import './ItemList.css'
 
-const ItemList = () => {
+// const ItemList = () => {
+//     const [getApi, setGetApi] = useState([])
+//     const [getImg, setGetImg] = useState()
+
+//     const fecthGetApi = async () => {
+//         const response = await fetch('https://rickandmortyapi.com/api/character')
+//         .then((response) => response.json())
+
+//         setGetApi(response.results)
+//     }
+
+//     const fecthGetImg = async () => {
+//         const response = await fetch('https://pokeapi.co/api/v2/pokemon/charizard')
+//         .then((response) => response.json())
+
+//         setGetImg(response.sprites.front_default)
+//     }
+
+//     useEffect(() => {
+//         fecthGetApi()
+//     }, [])
+
+//     useEffect(() => {
+//        if (getApi.length > 0) {
+//           fecthGetImg()
+//        }
+//     }, [getApi])
+
+
+
+//     return(
+//         <div className="Prueba">
+//             {
+//                 getApi.length > 0 ? (
+//                   <div>  
+//                     {getApi.slice('',2).map((Api) => (
+//                         <div key={Api.id}>
+//                             <h1>{Api.name}</h1>
+//                         </div>
+//                     ))}
+//                    </div> 
+//                 ) : (
+//                     <h1>Loading...</h1>
+//                 )
+//             }
+//             {
+//              (getImg && <img src={getImg} alt='Imagen de Pokemon'/>)
+//             }
+//         </div>
+//     )
+// }
+
+
+  const ItemList = () => {
     const [getApi, setGetApi] = useState([])
     const [getImg, setGetImg] = useState()
 
@@ -22,34 +76,32 @@ const ItemList = () => {
         fecthGetApi()
     }, [])
 
-    useEffect(() => {
-       if (getApi.length > 0) {
-          fecthGetImg()
-       }
+    useEffect(() =>{
+        if(getApi.length > 0) {
+            fecthGetImg()
+        }
     }, [getApi])
 
-
-
     return(
-        <div>
-            {
-                getApi.length > 0 ? (
-                  <div>  
-                    {getApi.slice('',2).map((Api) => (
-                        <div key={Api.id}>
-                            <h1>{Api.name}</h1>
-                        </div>
-                    ))}
-                   </div> 
-                ) : (
-                    <h1>Loading...</h1>
-                )
-            }
-            {
-             (getImg && <img src={getImg} alt='Imagen de Pokemon'/>)
-            }
-        </div>
+        <main className="Prueba">
+           {getApi.length > 0 ? (
+            <div>
+             {getApi.slice(' ', 1).map((character) => (
+                <div key={character.id}>
+                  <h1>{character.name}</h1>
+                </div>
+             ))}
+             </div> 
+             ) : (
+                <h1>Loading...</h1>
+             )
+           }
+           {
+            (getImg && <img src={getImg} alt='Pokemon Charizard'/>)
+           }
+
+        </main>
     )
-}
+  }
 
 export default ItemList
