28046ee79d3674dcbd7fd53e9b321bfe9c0bb01f
MartinMinuto <martin.minuto1@gmail.com>
2023-03-28 20:32:05 -0300

    Config

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,48 @@ import './App.css'
 import ItemDetailsContainer from './ItemDetails/ItemDetails'
 import ItemList from './ItemList/ItemList'
 
+import React, { useState, useEffect } from 'react';
+
+function DesktopView() {
+  return (
+    <div>
+        <ItemDetailsContainer/>
+    </div>
+  );
+}
+
+function MobileView() {
+  return (
+    <div>
+     <ItemList/>
+    </div>
+  );
+}
+
 function App() {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    function handleResize() {
+      if (window.innerWidth < 768) {
+        setIsMobile(true);
+      } else {
+        setIsMobile(false);
+      }
+    }
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
   return (
-    <div className="App">
-         <ItemDetailsContainer/>
-         <ItemList/>
+    <div>
+      {isMobile ? <MobileView /> : <DesktopView />}
     </div>
-  )
+  );
 }
 
 export default App
